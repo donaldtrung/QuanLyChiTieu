1cbc11e1fc3f837294c4fcdda1c101a8c42bb58e
1353026 <1353026@student.hcmus.edu.vn>
2016-03-26 13:15:21 +0700

    Change to error

--- MIB/Data.cs
+++ MIB/Data.cs
@@ -120,7 +120,7 @@ namespace MIB
                      newRow["Description"] = data[i - 1].describe;
                      tb.Rows.Add(newRow);
 
-                     sum -= money;
+                     sum += money;
                  }
                  //{
                  //    text = text + data[i - 1].time + "\r\n" + data[i - 1].money + " " + data[i - 1].unit + "\r\n" + data[i - 1].describe + "\r\n\r\n";
@@ -147,7 +147,7 @@ namespace MIB
         {
             GetStringData("revenue", ref sum_rev);
             GetStringData("expenditure", ref sum_exp);
-            balance = sum_rev - sum_exp;
+            balance = sum_rev + sum_exp;
         }
         private double CalMoney(string type_money, double money)
         {
