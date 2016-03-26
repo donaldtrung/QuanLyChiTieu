a96600c91aa7efa6f13a14b7356f5ba00736e4e0
1353026 <1353026@student.hcmus.edu.vn>
2016-03-26 12:42:48 +0700

    test again

--- Project.Test/TestDeafault.cs
+++ Project.Test/TestDeafault.cs
@@ -8,28 +8,5 @@ using System.Reflection;
 
 namespace Project.Test
 {
-    [TestFixture]
-    class TestDeafault
-    {
-        [Test]
-        public void CheckMonthDefault()
-        {
-            String scurMonth = DateTime.Now.Month.ToString();
-            scurMonth = "0" + scurMonth;
-            //String sMonth = curMonth.ToString();
-            MIB.Menux tmpForm = new MIB.Menux();
-            Assert.AreEqual(scurMonth, tmpForm.GetCBBMonth().Items[tmpForm.GetCBBMonth().SelectedIndex]);
-        }
-
-        [Test]
-        public void CheckYearDefault()
-        {
-            
-            String scurYear = DateTime.Now.Year.ToString();
-            //scurMonth = "0" + scurMonth;
-            //String sMonth = curMonth.ToString();
-            MIB.Menux tmpForm = new MIB.Menux();            
-           Assert.AreEqual(scurYear, tmpForm.GetCBBYear().Items[tmpForm.GetCBBYear().SelectedIndex]);
-        }
-    }
+    
 }
